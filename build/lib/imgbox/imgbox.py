b6c042c0551aa09984a1860c1f36b8a9b880d672
xxai.art <xxai.art@gmail.com>
2023-09-16 22:37:15 +0800

    ♨

--- build/lib/imgbox/imgbox.py
+++ build/lib/imgbox/imgbox.py
@@ -16,17 +16,12 @@ def imgbox(img, name_box_li):
   n = 0
   for (name, box) in name_box_li:
 
-    p1 = box[:2]
-    p2 = box[2:]
     n += 1
     c = (COLOR[n % 3] + 60) % 256
     COLOR[n % 3] = c
     color = COLOR[:]
     color[((n + 1 + n % 2) % 3)] = 255 - c
     color = tuple(color)
-    canvas.text([p1[0] + 5, p1[1] + 10], name, color, font=font)
-    canvas.rectangle(xy=(p1[0], p1[1], p1[0] + p2[0], p1[1] + p2[1]),
-                     fill=None,
-                     outline=color,
-                     width=1)
+    canvas.text([box[0] + 5, box[1] + 5], name, color, font=font)
+    canvas.rectangle(xy=box, fill=None, outline=color, width=1)
   return
