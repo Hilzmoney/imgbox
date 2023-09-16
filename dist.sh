9f3c33eb25997ac879e78ce43f0c4c7dfaf965f1
xxai.art <xxai.art@gmail.com>
2023-09-16 21:56:54 +0800

    ♨

--- dist.sh
+++ dist.sh
@@ -4,6 +4,10 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+if ! command -v bumpversion &>/dev/null; then
+  pip install bumpversion
+fi
+bumpversion patch
 rm -rf dist
 python setup.py sdist
 twine upload dist/*
